da63852d9195564c7b4b8395ffec841d5f90669f
Tsudo <49618663+The-Tech-Lord@users.noreply.github.com>
2024-10-21 21:04:25 -0400

    work

--- CS210_Retake/Assignments/Assignment 3 - percolation/src/Percolation.java
+++ CS210_Retake/Assignments/Assignment 3 - percolation/src/Percolation.java
@@ -3,41 +3,99 @@ import stdlib.In;
 import stdlib.StdOut;
 
 public class Percolation {
-    ...
+	WeightedQuickUnionUF uf_percolation_system;
+    boolean[][] percolation_system;  // false: blocked | true: open
+	int percolation_system_size;
+	int openSites;
 
     // Constructs an n x n percolation system, with all sites blocked.
     public Percolation(int n) {
-        ...
+		// Bounds checking
+        if (n <= 0)
+			throw new IllegalArgumentException("Illegal n");
+
+		// Instantiate variables
+		this.percolation_system_size = n;
+		this.percolation_system = new boolean[this.percolation_system_size][this.percolation_system_size];
+		this.uf_percolation_system = new WeightedQuickUnionUF(this.percolation_system_size * this.percolation_system_size + 2);
+
+		// Set the sites to a blocked state by setting to `false`
+		for (int i = 0; i < 0; i++)
+			for (int j = 0; j < 0; j++)
+				this.percolation_system[i][j] = false;
     }
 
     // Opens site (i, j) if it is not already open.
     public void open(int i, int j) {
-        ...
+		if (!this.isOpen(i, j)) {
+			this.percolation_system[i][j] = true;
+			this.openSites++;
+
+			// Check and connect site to either source or the sink
+			if (i == 0)
+				this.uf_percolation_system.union(0, this.encode(i, j));
+				
+			if (i == this.percolation_system_size - 1)
+				this.uf_percolation_system.union(this.percolation_system_size * this.percolation_system_size + 1, this.encode(i, j));
+
+			// Check for adjacent, open sites and connect them in `uf_percolation_system`
+			if (i - 1 > 0)
+				if (this.isOpen(i - 1, j))
+					this.uf_percolation_system.union(this.encode(i, j), this.encode(i - 1, j));
+			
+			if (j + 1 < this.percolation_system_size - 1)
+				if (this.isOpen(i, j + 1))
+					this.uf_percolation_system.union(this.encode(i, j), this.encode(i, j + 1));
+			
+			if (i + 1 < this.percolation_system_size - 1)
+				if (this.isOpen(i + 1, j))
+					this.uf_percolation_system.union(this.encode(i, j), this.encode(i + 1, j));
+			
+			if (j - 1 > 0)
+				if (this.isOpen(i, j - 1))
+					this.uf_percolation_system.union(this.encode(i, j), this.encode(i, j - 1));
+		}
     }
 
     // Returns true if site (i, j) is open, and false otherwise.
     public boolean isOpen(int i, int j) {
-        ...
+		// Checks if `i` and `j` are within bounds of `percolation_system[][]`
+        if (i < 0 || i > this.percolation_system_size - 1)
+			throw new IndexOutOfBoundsException("Illegal i or j");
+		if (j < 0 || j > this.percolation_system_size - 1)
+			throw new IndexOutOfBoundsException("Illegal i or j");
+
+		// Checks if the site is open
+		return this.percolation_system[i][j] == true;
     }
 
     // Returns true if site (i, j) is full, and false otherwise.
     public boolean isFull(int i, int j) {
-        ...
+        // Checks if `i` and `j` are within bounds of `percolation_system[][]`
+        if (i < 0 || i > this.percolation_system_size - 1)
+			throw new IndexOutOfBoundsException("Illegal i or j");
+		if (j < 0 || j > this.percolation_system_size - 1)
+			throw new IndexOutOfBoundsException("Illegal i or j");
+
+		// Check if site is open and connected to the source
+		return this.isOpen(i, j) && this.uf_percolation_system.connected(0, this.encode(i, j));
     }
 
     // Returns the number of open sites.
     public int numberOfOpenSites() {
-        ...
+        return this.openSites;
     }
 
     // Returns true if this system percolates, and false otherwise.
     public boolean percolates() {
-        ...
+		// Checks if a system percolates by checking the connection of the source and sink
+        return this.uf_percolation_system.connected(0, this.percolation_system_size + 1);
     }
 
     // Returns an integer ID (1...n) for site (i, j).
     private int encode(int i, int j) {
-        ...
+		// Converts an 2D array index into a union 1D index
+        return 1 + (i * this.percolation_system_size + j);
     }
 
     // Unit tests the data type. [DO NOT EDIT]
--- CS210_Retake/Assignments/Assignment 3 - percolation/src/PercolationStats.java
+++ CS210_Retake/Assignments/Assignment 3 - percolation/src/PercolationStats.java
@@ -3,31 +3,49 @@ import stdlib.StdRandom;
 import stdlib.StdStats;
 
 public class PercolationStats {
-    ...
+    private Percolation subject;
+	private int experiments;
+	private double[] thresholds;
 
     // Performs m independent experiments on an n x n percolation system.
     public PercolationStats(int n, int m) {
-        ...
+		if (n <= 0 || m <= 0)
+			throw new IllegalArgumentException("Illegal n or m");
+
+		// Initialize
+		this.subject = new Percolation(n);
+		this.experiments = m;
+        this.thresholds = new double[this.experiments];
+
+		for (int i = 0; i < m; i++) {
+			// Opening a random site and storing the percolation threshold in the index
+			// associated with the experiment number
+			this.subject.open(StdRandom.uniform(n), StdRandom.uniform(n));
+			this.thresholds[i] = (double)this.subject.numberOfOpenSites() / (double)(n * n);
+
+			if (this.subject.percolates())
+				break;
+		}
     }
 
     // Returns sample mean of percolation threshold.
     public double mean() {
-        ...
+		return StdStats.mean(this.thresholds);
     }
 
     // Returns sample standard deviation of percolation threshold.
     public double stddev() {
-        ...
+        return StdStats.stddev(this.thresholds);
     }
 
     // Returns low endpoint of the 95% confidence interval.
     public double confidenceLow() {
-        ...
+        return (this.mean() - ((1.96 * this.stddev()) / Math.sqrt(this.experiments)));
     }
 
     // Returns high endpoint of the 95% confidence interval.
     public double confidenceHigh() {
-        ...
+        return (this.mean() + ((1.96 * this.stddev()) / Math.sqrt(this.experiments)));
     }
 
     // Unit tests the data type. [DO NOT EDIT]
