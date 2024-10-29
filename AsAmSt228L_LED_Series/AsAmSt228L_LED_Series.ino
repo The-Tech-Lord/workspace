d26abd3aa82b0324bff7b69a85d0dea9356eacf7
Tsudo <49618663+The-Tech-Lord@users.noreply.github.com>
2024-10-29 12:18:55 -0400

    project

---
+++ AsAmSt228L_LED_Series/AsAmSt228L_LED_Series.ino
@@ -0,0 +1,28 @@
+void setup() {
+    // Main connections to LEDs
+    for (int i = 2; i <= 8; i++) {
+        pinMode(i, OUTPUT);
+    }
+
+    // Shorts the circuit to turn all LEDs off
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+    int delay_ms = 2000;
+
+    // Initialize pins 3-8 and 13 to LOW
+    for (int i = 2; i <= 8; i++) {
+        digitalWrite(i, LOW);
+    }
+
+    // It's show time BD
+    delay(delay_ms);
+    for (int i = 2; i <= 8; i++) {
+        digitalWrite(i, HIGH);
+        delay(delay_ms);
+    }
+
+    // Turn off all LEDs
+    delay(3000);
+}
