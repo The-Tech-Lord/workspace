e607bc282228235a1ac20d56117dc7b3bbff757c
The-Tech-Lord <49618663+The-Tech-Lord@users.noreply.github.com>
2025-03-26 11:32:35 -0400

    learned how to do quicksort

--- CS310/HW2/HW2_QuickSort.c
+++ CS310/HW2/HW2_QuickSort.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void partition(int* array, int arr_size, int begin, int end) {
-
+void swap(int* array, int index1, int index2) {
+	int temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
 }
 
-void quicksort(int* array, int arr_size) {
-	
+int partition(int* array, int start, int end) {
+	int pivot = array[end];
+	int index = start - 1;
+
+	for (int i = start; i <= end; i++)
+		printf("%d ", array[i]);
+	printf("\n%d\n", array[end]);
+
+	for (int i = start; i < end; i++) {
+		if (array[i] <= pivot) {
+			index++;
+			printf("Swapping (%d & %d): %d and %d\n", index, i, array[index],
+				   array[i]);
+			for (int j = start; j < end; j++)
+				printf("%d ", array[j]);
+			printf("\n");
+
+			swap(array, index, i);
+			
+			for (int j = start; j < end; j++)
+				printf("%d ", array[j]);
+			printf("\n");
+			printf("\n");
+		}
+	}
+	swap(array, index + 1, end);
+	for (int i = start; i < end; i++)
+		printf("%d ", array[i]);
+	printf("%d\n", array[end]);
+	return index + 1;
+}
+
+void quicksort(int* array, int start, int end) {
+	if (start < end) {
+		int split = partition(array, start, end);
+		quicksort(array, start, split - 1);
+		quicksort(array, split + 1, end);
+	}
 }
 
 int main() {
@@ -15,7 +53,7 @@ int main() {
 	printf("Enter array size: ");
 	scanf_s("%d", &array_size);
 
-	int *number_array = (int *)malloc(sizeof(int) * array_size);
+	int* number_array = (int *)malloc(sizeof(int) * array_size);
 	if (number_array == NULL) {
 		printf("Memory for number_array did not allocate properly...exiting");
 		return -1;
@@ -24,4 +62,9 @@ int main() {
 	printf("Enter elements: ");
 	for (int i = 0; i < array_size; i++)
 		scanf_s("%d", &number_array[i]);
+
+	quicksort(number_array, 0, array_size - 1);
+	for (int i = 0; i < array_size; i++)
+		printf("%d ", number_array[i]);
+	printf("\n");
 }
